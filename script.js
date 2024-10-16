0f243ebd2d8933bb16f98c4cb1da7c5998d30619
Zoey Bennett <zkb25@drexel.edu>
2024-10-16 05:09:25 -0400

    like/dislike flags

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let DislikeButton = document.querySelector('.post-rate:last-child');
-
 let LikeButton = document.querySelector('.post-rate:not(:last-child)')
+let LikeMessage = document.querySelector('.post-rating-selected.tooltiptext');
+let dislike = false;
+let LikeCount = 0;
+let like = false;
 
-
-
+let checkbox = document.getElementById("check");
+let LikeEle = document.getElementsByClassName("post-rating-like-count");
 
 
 // mouse down/up button
-
-
 
 let buttons = document.querySelectorAll('.post-rate')
 
@@ -17,8 +18,6 @@ buttons.forEach(button => {
     button.addEventListener('mousedown', () => {
         button.style.backgroundColor = '#bbbbbb'; // Change color to a darker gray
     });
-
-
     button.addEventListener('mouseup', () => {
         button.style.backgroundColor = ''; 
     });
@@ -39,18 +38,54 @@ DislikeButton.addEventListener('click', () => {
         DislikeIcon.classList.remove('material-symbols-outlined');
         // changes button to be filled in
         DislikeIcon.classList.add('material-icons');
+        dislike = true;
+
+        if (like === true) {
+            checkbox.checked = false;
+            like = false;
+            LikeCount -= 1;
+            LikeEle[0].textContent = LikeCount;
+
+
+        };
 
 
     } else {
         DislikeIcon.classList.remove('material-icons');
         // changes button to be filled in
         DislikeIcon.classList.add('material-symbols-outlined');
+        dislike = false;
+
     };
             
 
 });
 
-// Animation for like button
+// Animation and Like count for like button
+
+checkbox.addEventListener('change', function() {
+    LikeEle[0].textContent='';
+
+    if (checkbox.checked) {
+        LikeCount += 1; // Increment like count
+        like = true;
+        
+        if (dislike === true) {
+            DislikeIcon.classList.remove('material-icons');
+            // changes button to be filled in
+            DislikeIcon.classList.add('material-symbols-outlined');
+            dislike = false;
+        };
+
+    } else {
+        LikeCount -= 1; // Decrement like count
+        like = false;
+    }
+    LikeEle[0].textContent = LikeCount; // Update the displayed like count
+});
+
+
+
 
 
 
